c7b492b2125dd1bf5bceb66811c5d96d7140aac8
Penn Curtis <penn.s.m.c@gmail.com>
2023-06-14 14:24:41 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -44,6 +44,43 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        data = request.get_json()
+
+        plant = Plant.query.filter_by(id=id).first()
+
+        for attr in data:
+            setattr(plant, attr, data[attr])
+        
+        db.session.add(plant)
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return make_response(response)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response_dict = {"message": "delete delete delete"}
+
+        response = make_response(
+            response_dict,
+            204
+        )
+
+        return response
+
+        
 
 api.add_resource(PlantByID, '/plants/<int:id>')
         
